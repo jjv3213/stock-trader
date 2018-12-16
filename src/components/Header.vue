651996fadc9362ff5160957f68b1df339282b432
joelv <joelv3213@gmail.com>
2018-12-16 07:51:10 -0600

    add animation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,28 +17,7 @@
         <strong class="navbar-text navbar-right">Funds: {{funds | currency}}</strong>
         <ul class="nav navbar-nav navbar-right">
           <li>
-            <a href="#">End Day</a>
-          </li>
-          <li class="dropdown">
-            <a
-              href="#"
-              class="dropdown-toggle"
-              data-toggle="dropdown"
-              role="button"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Save & Load
-              <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <a href="#">Save Data</a>
-              </li>
-              <li>
-                <a href="#">Load Data</a>
-              </li>
-            </ul>
+            <a href="#" @click="endDay">End Day</a>
           </li>
         </ul>
       </div>
@@ -49,10 +28,17 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   computed: {
     funds() {
       return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    ...mapActions(["randomizeStocks"]),
+    endDay() {
+      this.randomizeStocks();
     }
   }
 };
